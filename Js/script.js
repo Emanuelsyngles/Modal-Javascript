e29459d202bc12d20e204f8ccd8cae716297ea6d
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-08-31 22:27:49 -0300

    modal

--- Js/script.js
+++ Js/script.js
@@ -1,11 +1,1 @@
-function openmodal() {
-  const modal = document.getElementById('janela-modal')
-  modal.classList.add('open')
-
-  modal.addEventListener('click', (e) => {
-    if(e.target.id == 'fechar' || e.target.id == 'janela-modal') {
-      modal.classList.remove('open')
-    }
-  })
-}
 
